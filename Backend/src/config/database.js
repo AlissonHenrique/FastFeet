a786f6b508266a2f73a62e962fae693303aa78f2
Alisson <alisson-henri@live.com>
2020-01-28 10:49:14 -0300

    erro no back

---
+++ Backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'rajje.db.elephantsql.com',
+  username: 'kkbfhqyj',
+  password: 'Xo35GKONxhPM8ado9ZLqSVxAHhmeLaxl',
+  database: 'kkbfhqyj',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
+import Usercontroller from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.send("ok")
-})
+routes.post('/users', Usercontroller.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 export default routes;
