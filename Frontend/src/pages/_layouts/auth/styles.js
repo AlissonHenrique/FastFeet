57f73d4ba23268393f1a2e4dd233a5de5f868d0d
Alisson Henrique <alisson-henri@live.com>
2020-02-20 21:10:01 -0300

    finish pre front

---
+++ Frontend/src/pages/_layouts/auth/styles.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: #ee4d64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 360px;
+  text-align: center;
+  background: #fff;
+  padding: 50px 40px;
+  border-radius: 4px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      border: 1px solid #ddd;
+
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #999;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: #999;
+      }
+    }
+
+    /* span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    } */
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #ee4d64;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#EE4D64')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      font-weight: bold;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
